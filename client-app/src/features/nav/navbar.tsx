36342d3bb743f5b71341a909c06d7ba314d855a6
Idris Yakub <idris.yakub@live.com>
2020-02-28 16:36:38 +0000

    Finished CRUD in client app without State Management

---
+++ client-app/src/features/nav/navbar.tsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+interface IProps{
+    openCreateForm: () => void;
+}
+const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+    return (
+        <Fragment>
+            <Menu fixed='top' inverted>
+                <Container> 
+                    <Menu.Item header>
+                        <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
+                        Reactivities
+                    </Menu.Item>
+                    <Menu.Item name='Activities' />
+                    <Menu.Item>
+                        <Button onClick={openCreateForm} positive content='Create Activity' />
+                    </Menu.Item>
+
+                </Container>
+            </Menu>
+        </Fragment>
+    );
+};
+export default NavBar;
