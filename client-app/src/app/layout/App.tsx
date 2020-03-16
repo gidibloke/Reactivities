c8d18e2876f04091e38287e48010aefd436c26b6
Idris Yakub <idris.yakub@live.com>
2020-03-16 14:08:46 +0000

    Styling added to project

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 import {observer} from 'mobx-react-lite';
 import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
-import { HomePage } from '../../features/home/HomePage';
+import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../../../../../Demo/Reactivities/client-app/src/features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
